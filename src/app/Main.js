1ed96c93593fddc2091db6254dfe35cfa72b6403
Richard Ding <richard@intelliwaves.com>
2018-04-01 15:24:46 -0400

    My first commit

---
+++ src/app/Main.js
@@ -0,0 +1,21 @@
+import 'babel-core/register';
+import 'babel-polyfill';
+
+var koa = require('koa');
+var app = module.exports = new koa();
+var routes = require('koa-route');
+
+//const greetings = async (ctx, next) => {
+//    ctx.body = 'self';
+//    await next();
+//    ctx.body += ' Remember to subscribe';
+//}
+//const app = new koa();
+app.use(routes.get("/user/:id",get));
+function *get(id) {
+  this.body = "you passsed me :" + id;
+};
+//const app = new koa();
+//app.use(greetings);
+app.listen(3000);
+console.log("The app is listening on port 3000");
---
+++ src/server/server.js
@@ -0,0 +1,8 @@
+var koa = require('koa');
+var app = new koa();
+
+app.use(function *(){
+    this.body = 'Hello from Koajs';
+})
+
+app.listen(3001);
